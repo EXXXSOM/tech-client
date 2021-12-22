08f72cea976d5e27a142a3f42a608d565a04a929
Anton Ryzhkov <avatoxa@yandex.ru>
2021-12-22 18:42:08 +0300

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <v-footer padless absolute>
+        </v-footer>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.v-footer {
+    flex-shrink: 0 !important;
+}
+.help {
+    height: 100px;
+    width: 100px;
+    margin-top: 100px;
+}
+</style>
